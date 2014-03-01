51e262a666d9b2b208ae2db69e8650c0235c792a
Horacio Ramirez <ramh31@gmail.com>
2014-02-28 22:27:29 -0600

    mod ediciones url dinamically

--- sites/all/themes/global_news/js/engine.js
+++ sites/all/themes/global_news/js/engine.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function(){
+jQuery(window).load(function(){
 	  jQuery('.view-ediciones .views-row').each(function(){
     	var url1 = jQuery(this).find('.taxonomy-term h2 a').css('color','red').attr('href').replace('ediciones','sanguis');
     	console.log(url1);
