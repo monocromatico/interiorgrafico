888abe02ec7cc942f6f95fb5a393ddaae5d9edf3
Horacio Ramirez <ramh31@gmail.com>
2014-03-03 13:14:00 -0600

    urls

--- sites/all/themes/global_news/js/engine.js
+++ sites/all/themes/global_news/js/engine.js
@@ -1,6 +1,6 @@
 jQuery(window).load(function(){
 	  jQuery('.view-ediciones .views-row').each(function(){
-    	var url1 = jQuery(this).find('a').css('color','red').prop('href').replace('ediciones','sanguis');
+    	var url1 = jQuery(this).find('a').css('color','red').attr('href').replace('ediciones','sanguis');
     	console.log(url1);
   });
 });
