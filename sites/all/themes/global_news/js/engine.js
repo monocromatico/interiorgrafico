749bd9bd3b698345c96bc8ff67d38b47e045ad9a
Horacio <ramh31@gmail.com>
2014-03-27 17:54:26 -0600

    frontpageig

--- sites/all/themes/global_news/js/engine.js
+++ sites/all/themes/global_news/js/engine.js
@@ -20,7 +20,7 @@ jQuery(window).load(function(){
      	$(this).find('.views-field-field-autores').html('');
      });
 
-     $('.view-id-ig_front .view-footer a').attr('href').html('meh').replace('ediciones','edicion');
+     $('.view-id-ig_front .view-footer a').attr('href').replace('ediciones','edicion').text('Ver más');
 
 
 })(jQuery);
